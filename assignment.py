77a61450120f2648584bb1fd9f9dc68f69ea62ca
djawlths3 <djawlths4@naver.com>
2019-06-12 13:48:02 +0900

    파이썬 문법 변수명과 예약어

---
+++ assignment.py
@@ -0,0 +1,19 @@
+# 치환문
+
+a = 1
+b = a + 1
+print(a, b)
+
+e = 3.5; f= 5.3
+print(e, '//', f)
+
+e, f= 3.5, 5.3
+print(e, '//', f)
+
+x = y = z = 10
+print(x, y, z)
+
+a = 1
+print(a, type(a))
+a = 'love'
+print(a, type(a))
---
+++ valiable.py
@@ -0,0 +1,12 @@
+# 변수 이름은 문자, 숫자,_으로 구성
+import keyword
+
+friend = 1
+a = 10
+my_name = '엄기윤'
+myName = '엄기윤'
+_yourname = '엄기윤'
+member1 = '엄기윤'
+
+print(keyword.kwlist)
+print(len(keyword.kwlist))
